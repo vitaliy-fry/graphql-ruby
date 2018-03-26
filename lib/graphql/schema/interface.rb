33630504e1b6b4061f37939d0dffe75813926d1d
Robert Mosolgo <rdmosolgo@gmail.com>
2018-03-26 09:25:49 -0400

    Inline code from AS::Concern

--- lib/graphql/schema/interface.rb
+++ lib/graphql/schema/interface.rb
@@ -2,7 +2,38 @@
 module GraphQL
   class Schema
     module Interface
-      extend ActiveSupport::Concern
+      # This is borrowed from ActiveSupport::Concern
+      # @api private
+      module Extendable
+        def self.extended(base)
+          base.instance_variable_set(:@_dependencies, [])
+        end
+
+        def append_features(base)
+          if base.instance_variable_defined?(:@_dependencies)
+            base.instance_variable_get(:@_dependencies) << self
+            false
+          else
+            return false if base < self
+            @_dependencies.each { |dep| base.include(dep) }
+            super
+            base.extend const_get(:ClassMethods) if const_defined?(:ClassMethods)
+            base.class_eval(&@_included_block) if instance_variable_defined?(:@_included_block)
+          end
+        end
+
+        def included(base = nil, &block)
+          if base.nil?
+            raise MultipleIncludedBlocks if instance_variable_defined?(:@_included_block)
+
+            @_included_block = block
+          else
+            super
+          end
+        end
+      end
+
+      extend Extendable
       include GraphQL::Schema::Member
       include GraphQL::Schema::Member::HasFields
       extend GraphQL::Schema::Member::AcceptsDefinition
@@ -31,10 +62,11 @@ module GraphQL
           # Here's the tricky part. Make sure behavior keeps making its way down the inheritance chain.
           def append_features(child_class)
             if !child_class.is_a?(Class)
+              # In this case, it's been included into another interface.
+              # This is how interface inheritance is implemented
               child_class.include(GraphQL::Schema::Interface)
               child_class.extend(GraphQL::Schema::Member::DSLMethods)
             end
-
             super
           end
 
